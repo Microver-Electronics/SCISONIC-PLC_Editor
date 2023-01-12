2c0c0b8b0a3e73c9ff6f08629623ba52c736c2d2
doğu alpay <dogualpayy+github@gmail.com>
2023-01-13 01:25:17 +0300

    final version
    now after the first setup, you can upgrade the software by using in app button.
    It just download our sources and the script to locate each source, then runs the script.

--- upgrade.cmd
+++ upgrade.cmd
@@ -2,11 +2,11 @@
 setlocal enabledelayedexpansion
 set user=%USERNAME%
 set cd=%CD%
-xcopy /y "%cd%\source\MicroBCM.cpp" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\src\hal\"
-xcopy /y "%cd%\source\MicroBCM.hal" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\src\hal\"
-xcopy /y "%cd%\source\builder.py" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\"
-xcopy /y "%cd%\source\hals.json" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\examples\Baremetal\"
-xcopy /y "%cd%\source\splash.png" "C:\Users\%user%\OpenPLC_Editor\editor\images\"
-xcopy /y "%cd%\source\arduino.png" "C:\Users\%user%\OpenPLC_Editor\editor\images\"
-xcopy /y "%cd%\source\revision" "C:\Users\%user%\OpenPLC_Editor\"
+xcopy /y /i "%cd%\source\MicroBCM.cpp" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\src\hal\"
+xcopy /y /i "%cd%\source\MicroBCM.hal" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\src\hal\"
+xcopy /y /i "%cd%\source\builder.py" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\"
+xcopy /y /i "%cd%\source\hals.json" "C:\Users\%user%\OpenPLC_Editor\editor\arduino\examples\Baremetal\"
+xcopy /y /i "%cd%\source\splash.png" "C:\Users\%user%\OpenPLC_Editor\editor\images\"
+xcopy /y /i "%cd%\source\arduino.png" "C:\Users\%user%\OpenPLC_Editor\editor\images\"
+xcopy /y /i "%cd%\source\revision" "C:\Users\%user%\OpenPLC_Editor\"
 echo Folder copied and overwritten successfully!
